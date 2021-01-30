d9405f449e76c72578a7e868164e4fda14e7bcc7
Robilko <75025794+Robilko@users.noreply.github.com>
2021-01-31 00:53:39 +0300

    Практическая работа к уроку 2

---
+++ lesson2/MyArrayDataException.java
@@ -0,0 +1,10 @@
+package Java_2_AdvancedLevel.lesson2;
+
+public class MyArrayDataException extends NumberFormatException{
+    public MyArrayDataException() {
+    }
+
+    public MyArrayDataException(String s) {
+        super(s);
+    }
+}
---
+++ lesson2/MyArraySizeException.java
@@ -0,0 +1,10 @@
+package Java_2_AdvancedLevel.lesson2;
+
+public  class MyArraySizeException extends Exception {
+    public MyArraySizeException() {
+    }
+
+    public MyArraySizeException(String s) {
+        super(s);
+    }
+}
